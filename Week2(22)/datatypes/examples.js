eb88164332e418ceb9170bff11cff4f951957bde
Dhanushri <dhanushriramasamy1809@gmail.com>
2026-06-22 19:31:48 +0530

    Week 2

---
+++ Week2(22)/datatypes/examples.js
@@ -0,0 +1,7 @@
+let name = "Dhanushri"; // String
+let age = 20;     // Number
+let student = true;   // Boolean
+let city;    // Undefined
+let data = 0;  // Null
+
+console.log(name + "\n" + age + "\n" + student + "\n" + city + "\n" + data);
