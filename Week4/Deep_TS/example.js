c1e61be510ebc60b5e68a59c93cf8e0994f50997
Dhanushri <dhanushriramasamy1809@gmail.com>
2026-07-06 19:18:36 +0530

    Typescript Deep concepts

---
+++ Week4/Deep_TS/example.js
@@ -0,0 +1,17 @@
+"use strict";
+// // Union Type
+// let studentId: number | string;
+// studentId = 101;
+// console.log(studentId);
+// studentId = "101";
+// console.log(studentId);
+// let studentId: string = "Dhanushri";
+// console.log(studentName);
+const numberBox = {
+    item: 100
+};
+const stringBox = {
+    item: "TypeScript"
+};
+console.log(numberBox);
+console.log(stringBox);
