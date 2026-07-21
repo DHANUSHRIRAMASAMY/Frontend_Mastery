478ed39140f240f9939572b527ac1e3573e9d9fd
Dhanushri <dhanushriramasamy1809@gmail.com>
2026-07-21 21:00:05 +0530

    NestSetup

--- Week6/Node/app.js
+++ Week6/Node/app.js
@@ -1,21 +1,22 @@
-// console.log("Hello Dhanu");
+console.log("Hello Dhanu");
 
-// //variables
-// const name ="Dhanu";
-// let age = 13;
-// console.log(name);
-// console.log(age);
+//variables
+const name ="Dhanu";
+let age = 13;
+console.log(name);
+console.log(age);
 
-// //function
-// function greet(name){
-//     return "Hello " + name ;
-// }
-// console.log(greet("Dhanu"));
+//function
+function greet(name){
+    return "Hello " + name ;
+}
+console.log(greet("Dhanu"));
 
-//modules
-// const add = require("./math");
-// console.log(add(3,5));
+// modules
+const add = require("./math");
+console.log(add(3,5));
 
+//function
 const fs = require("fs");
 fs.readFile("sample.txt","utf8",(err,data) =>{
     if (err){
---
+++ Week6/Node/npm/msg.js
@@ -0,0 +1,4 @@
+// npm
+const _ = require("lodash");
+const numbers = [1, 2, 3, 4];
+console.log(_.reverse(numbers));
